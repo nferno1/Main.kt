43c6bfd394f3d842faf7c2dcb65a6b97e44c0179
Roman Podkopaev <nferno1@mail.ru>
2022-12-04 11:13:28 +0700

    Kotlin type continuation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,38 @@
 fun main(args: Array<String>) {
 
-//var variable // ���������� ����������
-//val value // ������������ ���������� final
+    val longNumber1: Long = 999999999L
+    val floatNumber2: Float = 68.88f
+
+    val a: Boolean = true
+    val b: Boolean = false
+    val charSymbol1: Char = 'y'
+    val textString: String = "Kotlin - статически типизированный язык программирования, \n разбабатываемый компанией JetBrains"
+    println(textString)
+    val name = "Kotlin"
+    val age = 10
+    val description = "Laguage $name, age: $age"
+    println(description)
+
+    var  typeAny: Any = "Any type" // супертип всех типов
+    typeAny = 4858
+    println(typeAny)
+
+//var variable // изменяемые переменные
+//val value // неизменяемые переменные final
 
 
 
 
 }
+
+/*
+Byte: -128... 127 // 1 - байт
+Short: -32768.... 32767 // 2
+Int: -2.147.483.648.... 2.147.483.647 // 4
+Long: -9 223 372 036 854 775 808... 9 223 372 036 854 775 807 // 8
+Float: -3.4*1038... 3.4*1038 // 4
+Double: +-5.0*10-324... +-1.7*10308 // 8
+*/
 
 
 
